57cc699f78f2a8031b54fbc66ab163ecb9effe47
Sugam Thatal <sugam7051@gmail.com>
2026-01-12 13:23:02 +0545

    alternative method added

--- day2/customReact/react.js
+++ day2/customReact/react.js
@@ -12,10 +12,23 @@ const mainContainer=document.getElementById('root');
  
 function customRender(reactElement,container){
     const domElement=document.createElement(reactElement.type);
-    domElement.innerHTML=reactElement.children;
+    /*domElement.innerHTML=reactElement.children;
     domElement.setAttribute("href",reactElement.props.href);
     domElement.setAttribute("target",reactElement.props.target);
-    container.appendChild(domElement);          
+    container.appendChild(domElement);    */
+
+    //Alternative approach
+    for(const prop in reactElement.props){
+        domElement.setAttribute(prop,reactElement.props[prop]);
+    }
+    if(typeof reactElement.children==="string"){
+        domElement.innerHTML=reactElement.children;
+    }else if(Array.isArray(reactElement.children)){
+        reactElement.children.forEach(childElement=>{
+            customRender(childElement,domElement);
+        });
+    }
+    container.appendChild(domElement);      
 }
 
 customRender(reactElement,mainContainer);

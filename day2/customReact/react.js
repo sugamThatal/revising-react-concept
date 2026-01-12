632d9865606a1703ace5fb2230337cec7518e4af
Sugam Thatal <sugam7051@gmail.com>
2026-01-12 11:42:00 +0545

    customreact

---
+++ day2/customReact/react.js
@@ -0,0 +1,33 @@
+const reactElement={
+    type: "a",
+    props: {
+        target:"_blank",
+        href:"https://www.google.com",
+        
+    }
+    ,children:"Google"
+}
+
+const mainContainer=document.getElementById('root');
+ 
+function customRender(reactElement,container){
+    const domElement=document.createElement(reactElement.type);
+    domElement.innerHTML=reactElement.children;
+    domElement.setAttribute("href",reactElement.props.href);
+    domElement.setAttribute("target",reactElement.props.target);
+    container.appendChild(domElement);          
+}
+
+customRender(reactElement,mainContainer);
+/*<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="root"></div>
+    <script src="./react.js"></script>
+</body>
+</html> --></head> */

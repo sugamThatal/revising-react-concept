6d9bd4b3af6f9c2e48b8a9a835fa470bfca02d44
Sugam Thatal <sugam7051@gmail.com>
2026-01-12 09:55:17 +0545

    render test.jsx

---
+++ day2/reactApp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Test from './test.jsx'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Welcome to the React App</h1>
+      <Test />
+    </div>
+    
+  )
+}
+
+export default App

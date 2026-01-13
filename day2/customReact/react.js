ea1c377a6646ba467b77bea3815d57d0f811b1bc
Sugam Thatal <sugam7051@gmail.com>
2026-01-13 20:07:57 +0545

    update

--- day2/customReact/react.js
+++ day2/customReact/react.js
@@ -12,10 +12,10 @@ const mainContainer=document.getElementById('root');
  
 function customRender(reactElement,container){
     const domElement=document.createElement(reactElement.type);
-    /*domElement.innerHTML=reactElement.children;
+    domElement.innerHTML=reactElement.children;
     domElement.setAttribute("href",reactElement.props.href);
     domElement.setAttribute("target",reactElement.props.target);
-    container.appendChild(domElement);    */
+    container.appendChild(domElement);   }
 
     //Alternative approach
    /* for(const prop in reactElement.props){

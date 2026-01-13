256e8ab140f22ddfff520a283450770117163c96
Sugam Thatal <sugam7051@gmail.com>
2026-01-13 22:06:38 +0545

    main.jsx updated

--- day1/reactapp/fromvite/viteReactapp/src/main.jsx
+++ day1/reactapp/fromvite/viteReactapp/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+ 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const reactElement=(
+  <a href="https://www.google.com">Go to Google</a>
+)
+
+
+ //createRoot(document.getElementById('root')).render(
+   reactElement
+//)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+   reactElement 
+  
 )

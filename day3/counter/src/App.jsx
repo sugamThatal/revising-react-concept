0e6ef1355a6021ef759533bbf46f6f48ceb93f21
Sugam Thatal <sugam7051@gmail.com>
2026-01-14 21:20:12 +0545

    app.jsx modify

--- day3/counter/src/App.jsx
+++ day3/counter/src/App.jsx
@@ -5,29 +5,19 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  let counter=10;
+  const Addvalue=()=>{
+    counter=counter+1;
+    console.log(counter);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>React Counter</h1>
+     <h2>Counter value: {counter}</h2>
+     <button onClick={Addvalue}>Add Value</button>
+     <br />
+     <button>Remove value</button>
     </>
   )
 }
